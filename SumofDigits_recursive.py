844f6cb84687e61b8d4a1bb0748318f20b4d89aa
arjunmadduri <aadhavarjun.sa@gmail.com>
2024-04-17 20:32:17 -0700

    Added Comments

--- SumofDigits_recursive.py
+++ SumofDigits_recursive.py
@@ -1,10 +1,12 @@
-
+#function for breaking down the digits in the number
 def sum_of_digits_recursive(num):
+#if our input is = to 0 return 0
     if num == 0:
         return 0
+#if our input is not = to 0 use this formula to break down all the digits
     else:
         return (num % 10) + sum_of_digits_recursive(num // 10)
-
+#our input for giving the number with multiple digits to calculate the value of all of them combigned
 num = int(input("Enter a number:::"))
-
+#printing our final answer/result
 print ("The value of the all the digits combigned is", sum_of_digits_recursive(num))

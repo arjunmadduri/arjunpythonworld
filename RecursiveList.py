defba655816f295b1a7169b034329e1d20f9d75b
arjunmadduri <aadhavarjun.sa@gmail.com>
2024-04-21 17:17:32 -0700

    just adding comments to some comments to some codes

--- RecursiveList.py
+++ RecursiveList.py
@@ -1,10 +1,14 @@
+#this is our math library that we'll be adding just in case we need any built in functions from this particular library
 import math
-
+#this is our function that we will store our final result in
 def addition_of_list(list):
-    if not list:
+#this is the if condition saying that if our list has no value return the number 0
+    if not list:        
         return 0
+#this is our else condition saying that if our list has somesort of value we save this formula that is saying that our list starts at 0 adding every number above it and storing it in the function
     else:
         return list[0]+addition_of_list(list[1:])
-
+#this is our list we will be using to get an answer of how much the total value is of all the numbers inside it
 list = [1,2,3,4,5,6,7,8,9,10,11]
+#this is our final result when we are calling the formula from our function and giving our final result and caling the variable that is stored inside of our function
 print (addition_of_list(list))
--- power recursion.py
+++ power recursion.py
@@ -1,16 +1,16 @@
+#import math is the library we will be using just in case we need any built in functions to help us with our final result
 import math
-
+#this is the function that we'll be using to give our final output
 def power_recursion(x,y):
+#this is the if condition we'll be using to say that if our input is equal to 0 we will turn it in to 1 because if it is zero it will always give us 0 as our result 
     if y == 0:
-        #print("Intermediate result:", 1)
         return 1
+#here were saying that if our input is anything other than one save this formula that multiply's our variable of x by the function itself recalling it and subtracting one of y every time this happens 
     else:
-        #temp = power_recursion(x, y-1)
-        #result =  x * temp
-        #print(result)
         return x*power_recursion(x,y-1)
 
-
+#these are our inputs we'll be using to call in the function and also for an input
 x = int(input("Enter the number of x:::"))
 y = int(input("Enter the power:::"))
+#this is where we print our final ouput in a standard format
 print ("The power of x is", power_recursion(x,y))

b628b5fa0c1a93d8a2cf37df129a4e5c5555fbdd
arjunmadduri <aadhavarjun.sa@gmail.com>
2024-04-22 17:12:11 -0700

    just adding more explanation

--- SumofDigits_recursive.py
+++ SumofDigits_recursive.py
@@ -10,3 +10,19 @@ def sum_of_digits_recursive(num):
 num = int(input("Enter a number:::"))
 #printing our final answer/result
 print ("The value of the all the digits combigned is", sum_of_digits_recursive(num))
+
+
+
+
+''' 
+num=123
+123%10  ==3   123//10===12
+
+12%10== 2    12//10==1
+
+1%10==1       1//10==0
+
+
+3+2+1
+
+'''

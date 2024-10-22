e24898acb273b76861e548424c9231baca2e4826
arjunmadduri <aadhavarjun.sa@gmail.com>
2024-10-21 17:46:39 -0700

    Oct 21 2024 Some More codes

---
+++ Nov 2023/mode.py
@@ -0,0 +1,9 @@
+import statistics
+number = []
+number_amount = int(input("Enter how many number do you want to enter::: "))
+for i in range(number_amount):
+    numbers = int(input("Enter a number::: "))
+    number.append(numbers)
+print(number)
+mode = statistics.mode(number)
+print(mode)

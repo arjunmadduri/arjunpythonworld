490eea2231756a690a64c0f4d3b9979aad012119
arjunmadduri <aadhavarjun.sa@gmail.com>
2024-12-15 16:53:32 -0800

    turtle Programs

---
+++ 2024/basicsturtle.py
@@ -0,0 +1,178 @@
+#triangle
+# import turtle as t
+# t.lt(45)
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.rt(135)
+# t.fd(141)
+
+#square
+# import turtle as t
+# t.shape("turtle")
+# for i in range(2):
+#   t.fd(120)
+#   t.rt(90)
+#   t.fd(50)
+#   t.rt(90)
+# import random
+# colors = ["yellow","gray","black","red","orange"]
+# import turtle as t
+# t.shape("turtle")
+# t.color(random.choice(colors))
+# t.speed(500)
+# for i in range(100):
+#   t.begin_fill()
+#   t.fillcolor(random.choice(colors))
+#   t.circle(1)
+#   t.end_fill()
+#   t.lt(60)
+#   t.fd(70)
+#   t.bk(20)
+#   t.rt(20)
+#   t.fd(20)
+#   t.lt(35)
+#   t.fd(55)
+#   t.bk(20)
+#   t.lt(20)
+#   t.bk(30)
+#   t.rt(30)
+#   t.fd(20)
+#   t.bk(40)
+#   t.lt(95)
+#   t.fd(45)
+#   t.lt(70)
+#   t.bk(65)
+#   t.rt(55)
+#   t.fd(20)
+#   t.lt(75)
+#   t.bk(40)
+#   t.rt(65)
+#   t.bk(55)
+#   t.lt(60)
+#   t.fd(90)
+#   t.rt(75)
+#   t.bk(75)
+# #   t.fd(30)
+
+
+
+
+# import turtle as t
+# t.shape("turtle")
+# t.speed(500)
+# t.circle(50)
+# t.circle(-50,steps="3")
+# t.up()
+# t.goto(-20,65)
+# t.down()
+# t.circle(10)
+# t.begin_fill()
+# t.fillcolor("black")
+# t.circle(5)
+# t.end_fill()
+# t.up()
+# t.goto(20,65)
+# t.down()
+# t.circle(10)
+# t.begin_fill()
+# t.fillcolor("black")
+# t.circle("5")
+# t.end_fill()
+# t.up()
+# t.goto(0,65)
+# t.down()
+# t.rt(90)
+# t.fd(40)
+# t.lt(135)
+# t.fd(20)
+# t.up()
+# t.goto(0,0)
+# t.down()
+# t.up()
+# t.goto(20,-35)
+# t.down()
+# t.fd(50)
+# t.up()
+# t.goto(-20,-35)
+# t.down()
+# t.lt(90)
+# t.fd(50)
+# t.up()
+# t.goto(-20,-75)
+# t.down()
+# t.lt(135)
+# t.fd(60)
+# t.up()
+# t.goto(20,-75)
+# t.down()
+# t.fd(60)
+
+
+
+
+
+
+
+# import turtle as t
+# t.shape("turtle")
+# t.speed(500)
+# t.up()
+# t.goto(0,120)
+# t.down()
+# t.rt(90)
+# t.fd(240)
+# t.up()
+# t.goto(120,0)
+# t.down()
+# t.rt(90)
+# t.fd(240)
+# t.up()
+# t.goto(60,120)
+# t.down()
+# t.begin_fill()
+# t.fillcolor("orange")
+# t.circle(60)
+# t.end_fill()
+# t.up()
+# t.goto(-60,120)
+# t.down()
+# t.begin_fill()
+# t.fillcolor("red")
+# t.circle(60)
+# t.end_fill()
+# t.up()
+# t.goto(-120,-60)
+# t.down()
+# t.lt(90)
+# t.begin_fill()
+# t.fillcolor("yellow")
+# t.circle(60)
+# t.end_fill()
+# t.up()
+# t.goto(120,-60)
+# t.down()
+# t.rt(180)
+# t.begin_fill()
+# t.fillcolor("brown")
+# t.circle(60)
+# t.end_fill()
+
+
+
+
+
+
+# # how to make a turtle clone
+
+# import turtle as t 
+# t.shape("turtle")
+# t.speed(500)
+# t.fd(100)
+# x = t.clone()
+# x.shape("triangle")
+# x.bk(100)
+
+
+
+
